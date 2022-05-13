fbbb894921d5f1e48283d74168ff3c4f075587f4
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-05-13 18:24:53 +0530

    Program to copy all elements from an array to another array

--- Array_Copy.c
+++ Array_Copy.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
+void copy(int *src,int *dest,int n);
 int main()
- {
-   int arr1[30], arr2[30], i, num;
-   printf("\nEnter no of elements :");
-   scanf("%d",&num);
-   //Accepting values into Array
-   printf("\nEnter the values :");
-   for (i = 0; i < num; i++)
-   {
-      scanf("%d", &arr1[i]);
-   }
-   for (i = 0; i < num; i++)                  
-   {
-      arr2[i] = arr1[i];
-   }
- 
-   //Printing of all elements of array
-   printf("The copied array is :");
-   for (i = 0; i < num; i++)
-      printf("\narr2[%d] = %d", i, arr2[i]);
- 
-   return (0);
+{
+	int src[50],dest[50];
+	int n,i;
+	printf("Enter the size of array:");
+	scanf("%d",&n);
+	printf("Enter the elements of array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(src+i));
+	}
+	copy(src,dest,n);
+	printf("The copied elements of array are:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",*(src+i));
+	}
+  	printf("\n");
+  	return 0;
+}
+void copy(int *src,int *dest,int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		*(dest+i)=*(src+i);
+	}
 }
